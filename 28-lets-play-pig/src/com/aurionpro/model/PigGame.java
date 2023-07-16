9aab16d6a5f1612fad9958c6b1b5e7e20e3f96b2
Vedang Badawe <Vedang.Badawe@asl.mum.sst>
2023-07-16 20:38:38 +0530

    seventh commit

---
+++ 28-lets-play-pig/src/com/aurionpro/model/PigGame.java
@@ -0,0 +1,37 @@
+package com.aurionpro.model;
+
+public class PigGame {
+	private String action;
+	private int diceScore;
+	private int turnScore;
+	private int totalScore;
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public int getDiceScore() {
+		return diceScore;
+	}
+	public void setDiceScore(int diceScore) {
+		this.diceScore = diceScore;
+	}
+	public int getTurnScore() {
+		return turnScore;
+	}
+	public void setTurnScore(int turnScore) {
+		this.turnScore = turnScore;
+	}
+	public int getTotalScore() {
+		return totalScore;
+	}
+	public void setTotalScore(int totalScore) {
+		this.totalScore = totalScore;
+	}
+	
+	
+	
+	
+
+}
---
+++ 28-lets-play-pig/src/com/aurionpro/test/PigGameTest.java
@@ -0,0 +1,53 @@
+package com.aurionpro.test;
+import java.util.*;
+
+import com.aurionpro.model.PigGame;
+
+public class PigGameTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		
+		PigGame p1 = new PigGame();
+		
+		int turn = 1;
+		p1.setAction("roll");
+		int tuscore = 0;
+		int toscore=0;
+		
+		while(p1.getTotalScore()<20) {
+			System.out.println("Turn: "+turn);
+		do {
+		
+		System.out.println("Do you want to roll or hold?");
+		p1.setAction(sc.next());
+		
+		if(p1.getAction().equalsIgnoreCase("roll")){
+			p1.setDiceScore(rand.nextInt(6));
+			if(p1.getDiceScore()==1) {
+				System.out.println("Dice Score: "+p1.getDiceScore());
+				System.out.println("Turn over");
+				tuscore = 0;
+				break;
+			}else {
+			tuscore+=p1.getDiceScore();
+			System.out.println("Dice Score: "+p1.getDiceScore());
+			}
+				
+		}else {
+			break;
+		}
+		} while(p1.getAction().equalsIgnoreCase("roll") && p1.getTotalScore()<20);
+		p1.setTurnScore(tuscore);
+		System.out.println("Turn Score: "+p1.getTurnScore());
+		toscore+=p1.getTurnScore();
+		p1.setTotalScore(toscore);
+		System.out.println("Total Score: "+p1.getTotalScore());
+		turn++;
+
+	}
+		System.out.println("You win in: "+turn+ " turns");
+	}
+
+}
